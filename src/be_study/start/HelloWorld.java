36be507cb77ea3af891ec6d636c602fda1e845b1
tteugyul <yZQGCY0oHy0WrijW6cL7HQiHu2BKtt@gmail.com>
2026-05-06 13:57:30 +0900

    자바 시작, .gitignore 추가

---
+++ src/be_study/start/HelloWorld.java
@@ -0,0 +1,25 @@
+package be_study.start;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, World!!");
+		
+		// 자바 코드 실행 : Ctrl + F11
+		
+		// 주석 메모역할
+		
+		// 한줄 주석
+		/* 여러줄 코드
+		 * 엔터 치면서
+		 * 다음줄로내려가도
+		 * 주석으로 인식*/
+//		주석 	: 	Ctrl + /
+//				Ctrl + Shift + C
+		
+		System.out.println();
+	}
+
+}
