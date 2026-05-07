478497a57396d9d4a811744c152d313e0f5dfaa5
tteugyul <yZQGCY0oHy0WrijW6cL7HQiHu2BKtt@gmail.com>
2026-05-07 10:26:39 +0900

    퀴즈1 변수 기초

---
+++ src/be_study/quiz/Quiz01.java
@@ -0,0 +1,28 @@
+package be_study.quiz;
+
+public class Quiz01 {
+
+	public static void main(String[] args) {
+		int month;
+		int day = 1;
+		month = 5;
+		day = 7;
+		System.out.println("오늘은 " + month + "월 " + day + "일 입니다.");
+		String name = "김명현";
+		System.out.println(name);
+		String phoneNum = "010-5173-3635";
+		System.out.println("전화번호는 " + phoneNum + " 입니다.");
+//		a. 불가능 / 특수문자는 대부분의 변수명 시작 불가
+//		b. 불가능 / #은 변수명으로 사용불가
+//		c. 불가능 / 숫자는 변수명 시작으로 사용 불가
+//		d. 사용가능 / member에 시작부분인 m을 대문자로 표기하는것이 좋음
+//		e. 불가능 / 특수문자는 대부분의 변수명 시작 불가
+//		f. 불가능 / @는 변수명으로 사용 불가
+//		g. 불가능 / 예약어는 변수명으로 사용 불가
+
+//		C의 char 속성은 문자 하나를 저장하기에 잘못 되었음
+		
+//		b. int타입
+	}
+
+}
