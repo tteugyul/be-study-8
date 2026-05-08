70254dde30fca8dcc401e6efb399618f77b03924
tteugyul <yZQGCY0oHy0WrijW6cL7HQiHu2BKtt@gmail.com>
2026-05-08 12:35:22 +0900

    퀴즈 3 타입변환, 연산자

---
+++ src/be_study/quiz/Quiz03.java
@@ -0,0 +1,59 @@
+package be_study.quiz;
+
+public class Quiz03 {
+
+	public static void main(String[] args) {
+//		1. 아래 1), 2) 에 맞게 풀이.
+//		int x = 10;
+//		int y = 4;
+//		int result = x / y;
+//		System.out.println(result);
+
+//	1) 결과가 2가 나오도록 작성하세요
+//		int result = x / y;
+//		System.out.println(result);
+//	2) 결과가 2.5가 나오도록 작성하세요
+//		double result2 = (double) x / y;
+//		System.out.println(result2);
+
+//		2. 결과가 7.5가 나오도록 작성하세요
+//		double a = 3.5;
+//		double b = 4.7;
+//		double result = (a + b) - (0.7);
+//		System.out.println(result);
+
+//		3. 계산결과가 12가 나오도록 작성하세요
+//		String a = "3.4";
+//		String b = "4";
+//		int result = (int) (Double.parseDouble(a)) + Integer.parseInt(b) + 5;
+//		System.out.println(result);
+
+//		4. 계산결과가 "1013" 이 나오도록 코드를 작성하세요.
+//		String a = "10";
+//		int b = 3;
+//		double c = 4.5;
+//		String result = a + (b - 2) + ((int) c - 1);
+//		System.out.println(result);
+
+//		5. 계산결과가 "243" 이 나오도록 코드를 작성하세요.
+//		int a = 4;
+//		double b = 3.4;
+//		String c = "6.8";
+//		String result = String.valueOf(a - 2) + String.valueOf((int) b + 1) + (int) (Double.parseDouble(c) - 3);
+//		System.out.println(result);
+
+//		6. 계산 결과가 아래와 같이 나오도록 코드를 작성하세요.
+//		int x = 111;
+//		int y = 13;
+//		int result = _____;
+//		System.out.println(result);
+
+//		1) 출력결과 : 7
+//		int result = (x / y) - 1;
+//		System.out.println(result);
+//		2) 출력결과 : 8
+//		result = x / y;
+//		System.out.println(result);
+	}
+
+}
