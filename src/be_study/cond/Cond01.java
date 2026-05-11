9d2bc79c17d1e8bdaa44c4cf05880feb7f005c78
tteugyul <yZQGCY0oHy0WrijW6cL7HQiHu2BKtt@gmail.com>
2026-05-11 14:11:25 +0900

    조건문

---
+++ src/be_study/cond/Cond01.java
@@ -0,0 +1,18 @@
+package be_study.cond;
+
+public class Cond01 {
+
+	public static void main(String[] args) {
+
+//		조건문 if 문
+
+//		if(조건식) -> 조건식이 참이면 실행
+
+		int num = 15;
+		if (num > 50) {
+			System.out.println("num이 5보다 크다");
+			System.out.println("if문 안 {} 실행");
+		}
+	}
+
+}
