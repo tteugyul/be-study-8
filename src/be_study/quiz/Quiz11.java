a790007c2f0d690f20f7e3de6584e29eea0bb3c1
tteugyul <yZQGCY0oHy0WrijW6cL7HQiHu2BKtt@gmail.com>
2026-05-12 16:26:28 +0900

    퀴즈11 반복문

---
+++ src/be_study/quiz/Quiz11.java
@@ -0,0 +1,39 @@
+package be_study.quiz;
+
+public class Quiz11 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("12345");
+		}
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.println();
+
+			for (int j = 1; j <= 5; j++) {
+				System.out.print(j);
+			}
+		}
+
+		System.out.println();
+		System.out.println();
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.print(i);
+			System.out.print(i);
+			System.out.print(i);
+			System.out.print(i);
+			System.out.println(i);
+		}
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.println();
+
+			for (int j = 1; j <= 5; j++) {
+				System.out.print(i);
+			}
+		}
+
+	}
+
+}
